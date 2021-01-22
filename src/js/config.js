e88cd5c3339a810b9670ffa7089f8697f35a10c6
Muhammad-Tarek <muhammadtarek221@gmail.com>
2021-01-23 00:39:31 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const RES_PER_PAGE = 10;
+export const KEY = '2277be06-5141-4e27-92d6-5747da678459';
+export const MODAL_CLOSE_SEC = 2.5;
